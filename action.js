87d46d0a7f88daeedca28340905a22173c3abee4
danielefongo <danielefongo@gmail.com>
2020-04-20 13:58:42 +0200

    first commit

---
+++ action.js
@@ -0,0 +1,14 @@
+module.exports = function(loader, toggl, timeSlotter, asker, config) {
+  this.run = async () => {
+    a = await asker.autocompleteInquire('text', [
+      { name: "asd"},
+      { name: "basd"},
+      { name: "cacs"},
+      { name: "dadsds"},
+    ])
+    console.log(a)
+  }
+  this.help = () => {
+    return "Just Print 'Hello world!'"
+  }
+}
